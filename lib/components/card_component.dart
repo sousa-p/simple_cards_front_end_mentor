48fd89c20190d43929cc2b2014509ef905b1fa36
sousa-p <p.soousa7@gmail.com>
2023-12-02 15:47:47 -0300

    #

--- lib/components/card_component.dart
+++ lib/components/card_component.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
 
 class CardComponent extends StatelessWidget {
-  const CardComponent({super.key});
+  const CardComponent({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SizedBox(
+        width: 400,
+        height: 600,
+        child: Container(
+          color: Colors.amber,
+          child: const Padding(
+            padding: EdgeInsets.all(32),
+            child: Column(children: [
+              Text(
+                'SEDANS',
+                style: TextStyle(fontFamily: 'BigShouldersDisplay'),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.',
+                style: TextStyle(fontFamily: 'LexendDeca', fontSize: 15),
+              ),
+            ]),
+          ),
+        ));
   }
 }
--- lib/views/app_view.dart
+++ lib/views/app_view.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:simple_cards_front_end_mentor/components/card_component.dart';
 
 class AppView extends StatelessWidget {
   const AppView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      theme: ThemeData.light(),
+      home: const Scaffold(body: Center(child: CardComponent())),
+    );
   }
 }
