f6f6becd307f52db34e5617780826b74dcf32234
sousa-p <p.soousa7@gmail.com>
2023-12-01 23:22:23 -0300

    Criando componentes e views

---
+++ lib/components/card_component.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CardComponent extends StatelessWidget {
+  const CardComponent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
