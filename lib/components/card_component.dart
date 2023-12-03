164d5e0fd9187df263816dbeeaa8d13017791d70
sousa-p <p.soousa7@gmail.com>
2023-12-03 08:45:40 -0300

    Finalizando cards

--- lib/components/card_component.dart
+++ lib/components/card_component.dart
@@ -1,28 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class CardComponent extends StatelessWidget {
-  const CardComponent({Key? key}) : super(key: key);
+  final HSLColor background;
+  final String image;
+  final String title;
+  final String body;
+
+  const CardComponent(
+      {Key? key,
+      required this.background,
+      required this.title,
+      required this.body,
+      required this.image})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-        width: 400,
+        width: 375,
         height: 600,
         child: Container(
-          color: Colors.amber,
-          child: const Padding(
-            padding: EdgeInsets.all(32),
-            child: Column(children: [
-              Text(
-                'SEDANS',
-                style: TextStyle(fontFamily: 'BigShouldersDisplay'),
-              ),
-              SizedBox(height: 16),
-              Text(
-                'Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.',
-                style: TextStyle(fontFamily: 'LexendDeca', fontSize: 15),
-              ),
-            ]),
+          color: background.toColor(),
+          child: Padding(
+            padding: const EdgeInsets.all(64),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SvgPicture.asset(image, semanticsLabel: title),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 24),
+                    child: Text(title.toUpperCase(),
+                        style: const TextStyle(
+                          fontFamily: 'BigShouldersDisplay',
+                          fontSize: 48,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white,
+                        )),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 64),
+                    child: Text(
+                      body,
+                      style: TextStyle(
+                        fontFamily: 'LexendDeca',
+                        fontSize: 15,
+                        height: 2,
+                        letterSpacing: 1,
+                        color: const HSLColor.fromAHSL(1, 0, 0, 0.95).toColor(),
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(32)),
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 36, vertical: 24)),
+                      child: Text(
+                        'Learn More',
+                        style: TextStyle(
+                            color: background.toColor(), fontSize: 16),
+                      )),
+                ]),
           ),
         ));
   }
--- lib/views/app_view.dart
+++ lib/views/app_view.dart
@@ -8,7 +8,54 @@ class AppView extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.light(),
-      home: const Scaffold(body: Center(child: CardComponent())),
+      home: Scaffold(
+        body: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height / 7,
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: const Wrap(
+                    alignment: WrapAlignment.center,
+                    children: [
+                      CardComponent(
+                        background: HSLColor.fromAHSL(1, 31, 0.77, .52),
+                        image: 'assets/images/icon-sedans.svg',
+                        title: 'Sedans',
+                        body:
+                            'Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip',
+                      ),
+                      CardComponent(
+                        background: HSLColor.fromAHSL(1, 184, 1, .22),
+                        image: 'assets/images/icon-suvs.svg',
+                        title: 'SUVs',
+                        body:
+                            'Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.',
+                      ),
+                      CardComponent(
+                        background: HSLColor.fromAHSL(1, 179, 1, .13),
+                        image: 'assets/images/icon-luxury.svg',
+                        title: 'Luxury',
+                        body:
+                            'Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.',
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height / 7,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
