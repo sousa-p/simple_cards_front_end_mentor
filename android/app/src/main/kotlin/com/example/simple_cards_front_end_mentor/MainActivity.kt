cd36b8723bad48f13d80692cff093caf5516299c
sousa-p <p.soousa7@gmail.com>
2023-12-12 08:47:18 -0300

    Tentando colocar build

--- android/app/src/main/kotlin/com/example/simple_cards_front_end_mentor/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.simple_cards_front_end_mentor
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:simple_cards_front_end_mentor/views/app_view.dart';
-
-void main() {
-  runApp(const AppView());
-}
